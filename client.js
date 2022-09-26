6d349f06e0223930002cdafa2340fc457f83c3d6
Jakub Lewandowski <lewando54@gmail.com>
2022-09-26 23:39:10 +0200

    Nearly finished Main menu + Added first dashboard content section

--- client.js
+++ client.js
@@ -1,9 +1,11 @@
 document.querySelector('#hamburger').addEventListener('click', (e) => {
-    document.querySelector('#main-nav').classList.remove('left-full');
-    document.querySelector('#main-nav').classList.add('inset-0');
+    document.querySelector('#main-nav').classList.remove('-translate-x-full');
+    document.querySelector('#bg-fade-effect').classList.remove('opacity-0');
+    document.querySelector('#bg-fade-effect').classList.add('opacity-80');
 })
 
 document.querySelector('#close-main-nav').addEventListener('click', (e) => {
-    document.querySelector('#main-nav').classList.add('left-full');
-    document.querySelector('#main-nav').classList.remove('inset-0');
+    document.querySelector('#main-nav').classList.add('-translate-x-full');
+    document.querySelector('#bg-fade-effect').classList.remove('opacity-80');
+    document.querySelector('#bg-fade-effect').classList.add('opacity-0');
 })
